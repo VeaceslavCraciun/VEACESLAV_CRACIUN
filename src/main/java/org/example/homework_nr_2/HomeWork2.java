8e4bd912606e4c3d19fc09f57e27a64630ef049d
CraciunVeaceslav <tocraciun@gmail.com>
2023-03-22 09:31:24 +0200

    HW_nr_2 is done

--- src/main/java/org/example/homework_nr_2/HomeWork2.java
+++ src/main/java/org/example/homework_nr_2/HomeWork2.java
@@ -18,14 +18,15 @@ public class HomeWork2 {
             case 11 -> System.out.println("November");
             case 12 -> System.out.println("December");
         }
+        System.out.println();
         System.out.println("Цикл который отображает в консоль все числа от 100 до 1000 которые делятся на 5");
 
         for (int i = 100; i <=1000; i++) {
            if (i%5 == 0) System.out.println(i);
         }
         System.out.println();
-        //написать цикл(ы), который(е) отображает(ют) в консоль сумму следующей серии:
-        // 1/3 + 3/5 + 5/7 + 7/9 + 9/11 + 11/13 + ... + 95/97 + 97/99
+        System.out.println("написать цикл(ы), который(е) отображает(ют) в консоль сумму следующей серии:"
+        + "1/3 + 3/5 + 5/7 + 7/9 + 9/11 + 11/13 + ... + 95/97 + 97/99");
 
         double sum = 0.0;
         double n;
@@ -33,20 +34,18 @@ public class HomeWork2 {
             sum += n/(n + 2);
         {
             System.out.println("Сумма арифметической прогрессии равна: " + sum);
+
+        }
+
+        System.out.println();
+        System.out.println("Бонус:");
+
+        int i, j;
+            for(i = 0; i<= 8; i++){
+            for (j = 0; j<=i; j++)
+                System.out.print("*");
             System.out.println();
-
-        }
-
-        //Бонус: Используйте комбинацию из System.out.println и System.out.print
-        // и циклы для вывода на экран следующей фигуры
-
-        for(int i=0; i<3; i++){
-            System.out.println("a ");
-            for(int j=0; j<3; j++){
-            }
-            System.out.println("b");
-        }
-
+          }
         }
     }
 
