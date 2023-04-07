4e47c29d6867b2925f69e049836036e5c2c536cb
CraciunVeaceslav <tocraciun@gmail.com>
2023-04-07 12:34:13 +0300

    HW_nr_5 is done

---
+++ src/main/java/org/example/homework_nr_5/Months.java
@@ -0,0 +1,16 @@
+package org.example.homework_nr_5;
+
+public enum Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+}
